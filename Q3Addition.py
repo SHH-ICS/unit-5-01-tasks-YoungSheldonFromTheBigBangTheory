9573220c15f5cf089146d5a966cac4e781ffe9c8
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-04-26 14:51:32 +0000

    t

--- Q3Addition.py
+++ Q3Addition.py
@@ -1,2 +1,21 @@
 # Create a program that will ask the user an addition question. 
 # The program will generate two random numbers between 1 and 100, and display them as an addition question with appropriate prompts.
+import math
+import random
+
+while True:
+
+    a = random.randint(1, 100)
+    b = random.randint(1, 100)
+
+    y = input(str(a) + "+" + str(b) + "= ")
+
+    if int(y) == int(a+b):
+        print("Correct")
+    else:
+        print("Incorrect")
+
+    p = input("Play again. y/n: ")
+    if p == "n":
+        break
+    
