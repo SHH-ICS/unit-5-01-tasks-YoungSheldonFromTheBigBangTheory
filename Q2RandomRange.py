edb5c2a2b9514db6e6b79101b0f2a3a2bfa77ad8
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-04-26 14:40:53 +0000

    Q2

--- Q2RandomRange.py
+++ Q2RandomRange.py
@@ -1,2 +1,28 @@
 # Create a program that accepts 2 numbers from the user. 
 # Your program will output a random number between the range given by the user.
+import math
+import random
+while True:
+    a = input("Input 1st number: ")
+    try:
+        a = int(a)
+    except ValueError:
+        print("\nInvalid input.\n")
+        continue
+
+    b = input("Input 2nd number: ")
+    try:
+        b = int(b)
+    except ValueError:
+        print("\nInvalid input.\n")
+        continue
+
+
+    a2 = min(a, b)
+    b2 = max(a, b)
+
+    x = random.randrange(a2, b2)
+
+    print("\n" + str(x))
+
+    break
