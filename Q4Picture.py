85b69b5ab0e8391d93fd701bba37a0f9254e8072
YoungSheldonFromTheBigBangTheory <124380188+YoungSheldonFromTheBigBangTheory@users.noreply.github.com>
2023-05-12 13:08:17 +0000

    p

--- Q4Picture.py
+++ Q4Picture.py
@@ -15,9 +15,19 @@ while True:
             sys.exit()
     
     # Code to render stuff
-    pygame.draw.circle(Display, (255,0,0), (250, 200), 50)
-    pygame.draw.line(Display, (0, 0, 255), (250, 0), (250, 500))
-    pygame.draw.line(Display, (0, 0, 255), (0, 250), (500, 250))
+
+  
+    pygame.draw.rect(Display, (173,216,230), pygame.Rect(0, 0, 500, 400))
+    pygame.draw.rect(Display, (0,255,0), pygame.Rect(0, 400, 500, 400))
+  
+    pygame.draw.circle(Display, (255,255,0), (500, 0), 75)
+    pygame.draw.rect(Display, (150,75,0), pygame.Rect(100, 200, 300, 200))
+    pygame.draw.polygon(Display, (0,0,139), [(100, 200), (400, 200), (250,50)])
+    
+    
+   
+    #pygame.draw.line(Display, (0, 0, 255), (250, 0), (250, 500))
+    #pygame.draw.line(Display, (0, 0, 255), (0, 250), (500, 250))
   
     pygame.display.flip()
     Clock.tick(FPS)
